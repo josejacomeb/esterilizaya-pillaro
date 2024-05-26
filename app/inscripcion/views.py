a8eacd4fe8c30902755949c2913244a4431a9c51
José Jácome <josejacomeb@gmail.com>
2024-05-26 06:25:39 -0500

    tarea: crear una plantilla base

--- app/inscripcion/views.py
+++ app/inscripcion/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'inscripcion/index.html')
+    return render(request, 'index.html')
 
