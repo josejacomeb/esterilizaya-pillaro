72164310076cad711a9c28b6189cb0c0015bab0a
José Jácome <josejacomeb@gmail.com>
2024-10-18 23:07:37 -0500

    inscripciones: usar el objeto campana automáticamente

--- app/inscripcion/urls.py
+++ app/inscripcion/urls.py
@@ -4,10 +4,6 @@ from . import views
 
 app_name = "inscripcion"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("crear", views.crear, name="create"),
-    path("ver/<int:inscr_id>", views.index, name="show"),
-    path("guardar", views.index, name="store"),
-    path("editar/<int:inscr_id>", views.index, name="edit"),
-    # TODO: Borrar, modificar
+    path("<int:campana_id>/", views.index, name="index"),
+    path("crear/<int:campana_id>/", views.crear, name="create"),
 ]
--- app/inscripcion/views.py
+++ app/inscripcion/views.py
@@ -1,7 +1,8 @@
 import logging
 
 from campana.models import Campana
-from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import InscripcionForm
 from .models import Inscripcion
@@ -9,22 +10,25 @@ from .models import Inscripcion
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    inscripciones = Inscripcion.objects.all()
-    return render(request, "inscripcion/todos.html", {"inscripciones": inscripciones})
+def index(request, campana_id):
+    inscripciones = Inscripcion.objects.filter(campana_id=campana_id)
+    campana = inscripciones[0].campana
+    return render(request, "inscripcion/todos.html", {"inscripciones": inscripciones, "campana": campana})
 
 
-def crear(request):
-    # TODO: Mandar a crear una nueva campaña sino existe
+def crear(request, campana_id):
     if request.method == "POST":
         forma = InscripcionForm(request.POST)
         if forma.is_valid():
-            campana_id = request.POST.get("campana_id")
+            campana = get_object_or_404(Campana, id=campana_id, estado=Campana.Estado.ACTIVA)
+            usuario = User.objects.get(username=request.user)
             nueva_inscripcion = forma.save(commit=False)
 
-            nueva_inscripcion.campana_id = campana_id
+            nueva_inscripcion.campana = campana
+            nueva_inscripcion.usuario = usuario
+
             nueva_inscripcion.save()
-            return redirect("inscripcion:index")
+            return redirect("inscripcion:index", campana_id)
         else:
             forma.clean()
     else:
