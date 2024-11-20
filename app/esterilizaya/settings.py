07881d2b2ce7a285b03d3098cb7b0f1e3d9ee4f9
José Jácome <josejacomeb@gmail.com>
2024-11-19 22:26:13 -0500

    contenedores:  arreglo permisos

--- app/esterilizaya/settings.py
+++ app/esterilizaya/settings.py
@@ -158,12 +158,6 @@ LOGGING = {
         },
     },
     "handlers": {
-        "file": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": os.path.join(BASE_DIR, "debug.log"),
-            "formatter": "verbose",
-        },
         "console": {
             "level": "INFO",
             "class": "logging.StreamHandler",
@@ -172,12 +166,12 @@ LOGGING = {
     },
     "loggers": {
         "django": {
-            "handlers": ["file", "console"],
+            "handlers": ["console"],
             "level": "INFO",
             "propagate": True,
         },
         "root": {
-            "handlers": ["file", "console"],
+            "handlers": ["console"],
             "level": "INFO",
             "propagate": True,
         },
