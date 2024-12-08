eb969505f32a68e1c4c1afd0e3cb82ec14facdcb
José Jácome <josejacomeb@gmail.com>
2024-12-08 18:35:45 -0500

    registro: tener en cuenta ela campana

--- app/registro/forms.py
+++ app/registro/forms.py
@@ -15,12 +15,15 @@ class RegistroForm(forms.ModelForm):
         exclude = ["inscripcion", "usuario"]
 
     def __init__(self, *args, **kwargs):
+        self.inscripcion_campana_id = kwargs.pop("inscripcion_campana_id", None)
         super().__init__(*args, **kwargs)
         self.fields["raza_mascota"].initial = "Mestizo"
 
     def clean_numero_turno(self):
         numero_turno = self.cleaned_data.get("numero_turno")
-        if Registro.objects.filter(numero_turno=numero_turno).exists():
+        if Registro.objects.filter(
+            numero_turno=numero_turno, inscripcion__campana__id=self.inscripcion_campana_id
+        ).exists():
             raise forms.ValidationError(
                 f"El turno {numero_turno} ya ha sido asignado a otro tutor, agregue el turno correcto."
             )
