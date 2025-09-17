e42ea9a584233ca5f45f0cd17f7a70f9d112e97b
José Jácome <josejacomeb@gmail.com>
2025-09-17 16:21:24 -0500

    tarea: mejoras visuales campaña

--- app/campana/views.py
+++ app/campana/views.py
@@ -3,10 +3,12 @@ import logging
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from esterilizaya.constantes import PARROQUIAS_CANTON
 from inscripcion.models import Inscripcion
+from registro.models import Registro
 
 from .forms import CampanaForm
 from .models import Campana
@@ -38,10 +40,14 @@ def index(request):
 
 
 def mostrar(request, anio, parroquia, mes, dia):
-    logger.info(f"Anio: {anio}, parroquia: {parroquia}, mes: {mes}, dia: {dia}, {Campana.objects.all()}")
     campana = get_object_or_404(Campana, fecha__year=anio, parroquia=parroquia, fecha__month=mes, fecha__day=dia)
-    n_registrados = Inscripcion.objects.filter(campana_id=campana.id).count()
-    return render(request, "campana/campana.html", {"campana": campana, "n_registrados": n_registrados})
+    n_inscritos = Inscripcion.objects.filter(campana_id=campana.id).aggregate(total=Sum("cupos_totales"))
+    n_registrados = Registro.objects.filter(inscripcion__campana_id=campana.id).count()
+    return render(
+        request,
+        "campana/campana.html",
+        {"campana": campana, "n_registrados": n_registrados, "n_inscritos": n_inscritos["total"]},
+    )
 
 
 def listar_canton_parroquia(request):
