38dbe32f3eeca4c9480efe37b514e828f5bc89a7
José Jácome <josejacomeb@gmail.com>
2025-09-17 19:14:55 -0500

    tarea: mejoras campaña - Acceder a certificados, recetas, vistas veterinarios en la misma campaña - Añadir información de la campaña con una lista - Ordenar en orden inverso

--- app/inicio/views.py
+++ app/inicio/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     activas = Campana.activas.all()
-    pasadas = Campana.pasadas.all()
+    pasadas = Campana.pasadas.all().order_by("-fecha")
 
     return render(request, "inicio/inicio.html", {"activas": activas, "pasadas": pasadas})
