b779d5eb239619ed8ea0c9dbc5cfbead0905da44
José Jácome <josejacomeb@gmail.com>
2025-10-16 16:18:11 -0500

    wip: añadir debugbar

--- app/esterilizaya/settings/local.py
+++ app/esterilizaya/settings/local.py
@@ -1,10 +1,22 @@
+import os
+
 from .base import *
 from .utils import retornar_urls
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+LOCAL_IPS = ["localhost", "127.0.0.1"]
 
-ALLOWED_HOSTS = [retornar_urls()[-1], "localhost"]
+ALLOWED_HOSTS = [retornar_urls()[-1]] + LOCAL_IPS
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Python Debug Toolbar
+if DEBUG:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+    INTERNAL_IPS = LOCAL_IPS
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": "debug_toolbar.middleware.show_toolbar_with_docker",
+    }
--- app/esterilizaya/urls.py
+++ app/esterilizaya/urls.py
@@ -32,3 +32,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()
